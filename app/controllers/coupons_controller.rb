dd55386f6f408fe7d42b7bf2f277d13c816c952e
Jasmine Carter <jasmine.carter87@gmail.com>
2020-10-03 01:47:06 +0000

    Automatically backed up by Learn

--- app/controllers/coupons_controller.rb
+++ app/controllers/coupons_controller.rb
@@ -14,7 +14,7 @@ class CouponsController < ApplicationController
     @coupon.coupon_code = params[:coupon_code]
     @coupon.store = params[:store]
     @coupon.save
-    redirect_to coupon_path
+    redirect_to coupon_path(@coupon)
   end
 
 end
